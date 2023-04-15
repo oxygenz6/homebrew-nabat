0438ca6413048f578eec9f685d10fbac787cced9
oxygenz6 <oxygenz6+community@proton.me>
2023-04-16 03:00:46 +0330

    dnscrypt-proxy 2.1.4 (new formula)

---
+++ Formula/dnscrypt-proxy2.rb
@@ -0,0 +1,17 @@
+class DnscryptProxy2 < Formula
+  desc "Flexible DNS proxy, with support for encrypted DNS protocols"
+  homepage "https://dnscrypt.info"
+  url "https://github.com/DNSCrypt/dnscrypt-proxy/archive/refs/tags/2.1.4.tar.gz"
+  sha256 "05f0a3e8c8f489caf95919e2a75a1ec4598edd3428d2b9dd357caba6adb2607d"
+  license ""
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w", mod: "vendor")
+  end
+
+  test do
+    system "false"
+  end
+end
