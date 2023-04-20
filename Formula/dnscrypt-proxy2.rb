5fc7dcc50fe6940f9181ddd97ace2a9b713e7c71
oxygenz6 <oxygenz6+community@proton.me>
2023-04-21 02:48:08 +0330

    fix: replace dnscrypt-proxy with dnscrypt-proxy2

--- Formula/dnscrypt-proxy2.rb
+++ Formula/dnscrypt-proxy2.rb
@@ -1,4 +1,4 @@
-class DnscryptProxy < Formula
+class DnscryptProxy2 < Formula
   desc "Secure communications between a client and a DNS resolver"
   homepage "https://dnscrypt.info"
   url "https://github.com/DNSCrypt/dnscrypt-proxy/archive/2.1.4.tar.gz"
@@ -27,9 +27,9 @@ class DnscryptProxy < Formula
   def install
     cd "dnscrypt-proxy" do
       system "go", "build", "-ldflags", "-X main.version=#{version}", "-o",
-             sbin/"dnscrypt-proxy"
+             sbin/"dnscrypt-proxy2"
       pkgshare.install Dir["example*"]
-      etc.install pkgshare/"example-dnscrypt-proxy.toml" => "dnscrypt-proxy.toml"
+      etc.install pkgshare/"example-dnscrypt-proxy.toml" => "dnscrypt-proxy2.toml"
     end
   end
 
@@ -44,7 +44,7 @@ class DnscryptProxy < Formula
       By default, dnscrypt-proxy runs on localhost (127.0.0.1), port 53,
       balancing traffic across a set of resolvers. If you would like to
       change these settings, you will have to edit the configuration file:
-        #{etc}/dnscrypt-proxy.toml
+        #{etc}/dnscrypt-proxy2.toml
 
       To check that dnscrypt-proxy is working correctly, open Terminal and enter the
       following command. Replace en1 with whatever network interface you're using:
@@ -58,16 +58,16 @@ class DnscryptProxy < Formula
   end
 
   service do
-    run [opt_sbin/"dnscrypt-proxy", "-config", etc/"dnscrypt-proxy.toml"]
+    run [opt_sbin/"dnscrypt-proxy2", "-config", etc/"dnscrypt-proxy2.toml"]
     keep_alive true
     require_root true
   end
 
   test do
-    assert_match version.to_s, shell_output("#{sbin}/dnscrypt-proxy --version")
+    assert_match version.to_s, shell_output("#{sbin}/dnscrypt-proxy2 --version")
 
-    config = "-config #{etc}/dnscrypt-proxy.toml"
-    output = shell_output("#{sbin}/dnscrypt-proxy #{config} -list 2>&1")
+    config = "-config #{etc}/dnscrypt-proxy2.toml"
+    output = shell_output("#{sbin}/dnscrypt-proxy2 #{config} -list 2>&1")
     assert_match "Source [public-resolvers] loaded", output
   end
 end
